cf935a8cd93ad601279f4e4e092aa4436520c7b8
Sebastien Ruelle <sebast.ruelle@gmail.com>
2016-03-16 11:43:10 +0100

    1ere version, test

---
+++ MeteoKevina/src/donnee/Vent.java
@@ -0,0 +1,147 @@
+package donnee;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+import org.openstreetmap.gui.jmapviewer.Coordinate;
+import org.openstreetmap.gui.jmapviewer.Layer;
+import org.openstreetmap.gui.jmapviewer.MapPolygonImpl;
+
+public class Vent {
+
+	double direction;
+	double noeud;
+	Coordinate coord;
+	Layer layer;
+	
+	public Vent(Layer layer, Coordinate coord, double noeud, double direction) {
+		this.coord = coord;
+		this.direction = direction;
+		this.noeud = noeud;
+		this.layer = layer;
+	}
+	
+	public double getDirection() {
+		return direction;
+	}
+	public void setDirection(double direction) {
+		this.direction = direction;
+	}
+	public double getNoeud() {
+		return noeud;
+	}
+	public void setNoeud(double noeud) {
+		this.noeud = noeud;
+	}
+	public Coordinate getCoord() {
+		return coord;
+	}
+	public void setCoord(Coordinate coord) {
+		this.coord = coord;
+	}
+	public int getBeauford() {
+		
+		//TODO
+		return 0;
+	}
+	
+	public Coordinate[] getpoint(){
+		double zoom = 0.4;
+		double dir = direction*Math.PI/180;
+		double effetZoom = 1.5;
+		double effetAngl = 25;
+		double effetElem = 1.0;
+		double sousEffetZoom = 0.2;
+		double sousEffetAngl = 7;
+		double sousEffetElem = 0.25;
+		double n = noeud;
+		ArrayList<Coordinate> list = new ArrayList<Coordinate>();
+		
+		//TODO Si vent < a 5
+		
+		Coordinate c = coord;
+		list.add(c);
+		c = new Coordinate(coord.getLat() - Math.cos(dir)*zoom, coord.getLon() - Math.sin(dir)*zoom);
+		list.add(c);
+		c = coord;
+		list.add(c);
+		c = new Coordinate(coord.getLat() + Math.cos(dir)*zoom, coord.getLon() + Math.sin(dir)*zoom);
+		list.add(c);
+		
+		while (n >= 50) {
+			if (effetZoom == 1.5) effetZoom = 1.00;
+			//System.out.println("Triangle"); 
+			c = new Coordinate(coord.getLat() + Math.cos((direction+effetAngl)*Math.PI/180)*zoom*effetZoom, coord.getLon() + Math.sin((direction+effetAngl)*Math.PI/180)*zoom*effetZoom);
+			list.add(c);
+			effetElem -= 0.2;
+			c = new Coordinate(coord.getLat() + Math.cos(dir)*zoom*effetElem, coord.getLon() + Math.sin(dir)*zoom*effetElem);
+			list.add(c);
+			n-=50;
+			if (n >= 50){
+				effetElem -= 0.0;
+				c = new Coordinate(coord.getLat() + Math.cos(dir)*zoom*effetElem, coord.getLon() + Math.sin(dir)*zoom*effetElem);
+				list.add(c);
+				effetAngl += 8;
+				effetZoom -= 0.15;
+			}else {
+				effetElem -= 0.10;
+				c = new Coordinate(coord.getLat() + Math.cos(dir)*zoom*effetElem, coord.getLon() + Math.sin(dir)*zoom*effetElem);
+				list.add(c);
+				effetAngl += 7;
+				effetZoom -= 0.1;
+			}
+			sousEffetElem = 0.15;
+			sousEffetAngl = 10;
+			sousEffetZoom = 0.07;
+		}
+		while (n >= 10) {
+			//System.out.println("Longue");
+			c = new Coordinate(coord.getLat() + Math.cos((direction+effetAngl)*Math.PI/180)*zoom*effetZoom, coord.getLon() + Math.sin((direction+effetAngl)*Math.PI/180)*zoom*effetZoom);
+			list.add(c);
+			c = new Coordinate(coord.getLat() + Math.cos(dir)*zoom*effetElem, coord.getLon() + Math.sin(dir)*zoom*effetElem);
+			list.add(c);
+			effetElem -= sousEffetElem;
+			c = new Coordinate(coord.getLat() + Math.cos(dir)*zoom*effetElem, coord.getLon() + Math.sin(dir)*zoom*effetElem);
+			list.add(c);
+			effetAngl += sousEffetAngl;
+			effetZoom -= sousEffetZoom;
+			/*
+			c = new Coordinate(coord.getLat() + Math.cos((direction+34)*Math.PI/180)*zoom*0.95, coord.getLon() + Math.sin((direction+34)*Math.PI/180)*zoom*0.95);
+			list.add(c);
+			c = new Coordinate(coord.getLat() + Math.cos(dir)*zoom*0.7, coord.getLon() + Math.sin(dir)*zoom*0.7);
+			list.add(c);
+			c = new Coordinate(coord.getLat() + Math.cos(dir)*zoom*0.65, coord.getLon() + Math.sin(dir)*zoom*0.65);
+			list.add(c);
+			*/
+			n-=10;
+		}
+		while (n >= 5 ) {
+			//System.out.println("Courte");   n-=5; 
+		}
+
+		//c = new Coordinate(coord.getLat() + Math.cos((direction+25)*Math.PI/180)*zoom*1.5, coord.getLon() + Math.sin((direction+25)*Math.PI/180)*zoom*1.5);
+		//list.add(c);
+		//c = new Coordinate(coord.getLat() + Math.cos(dir)*zoom, coord.getLon() + Math.sin(dir)*zoom);
+		//list.add(c);
+		c = coord;
+		list.add(c);
+		
+		
+		Coordinate[] tab = new Coordinate[list.size()];
+		for (int i = 0 ; i < list.size() ; i++) {
+			tab[i] = list.get(i);
+		} 
+		return  tab;
+	}
+	
+	public MapPolygonImpl getPoly(){
+
+		
+		MapPolygonImpl fleche = new MapPolygonImpl(getpoint());
+		fleche.setBackColor(new Color(0.0f, 0.0f, 0.0f, 1.0f));
+		fleche.setColor(Color.BLACK);
+		fleche.setLayer(layer);
+		return fleche;
+	}
+	
+}
---
+++ MeteoKevina/src/grib/GribFile.java
@@ -0,0 +1,53 @@
+
+package grib;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.openstreetmap.gui.jmapviewer.Coordinate;
+import org.openstreetmap.gui.jmapviewer.Layer;
+
+import donnee.Vent;
+
+public class GribFile {
+
+	
+	public ArrayList<Vent> getVent(Layer layer){
+		ArrayList<Vent> list = new ArrayList<Vent>();
+		Random rand = new Random();
+		double force;
+		double angle;
+		for (double i = 0 ; i < 20 ; i++){
+			for (double j = 0; j < 40; j++) {
+				//force = 10 + rand.nextDouble() * 50;
+				//angle = rand.nextDouble() * 360;
+				//System.out.println("F = "+force+" A = "+angle);
+				list.add(new Vent(layer, c(42+i,-30+j), 30, 40));
+			}
+		}
+		
+		/*
+		list.add(new Vent(layer, c(0,0)    ,  10.0 , 0));
+		list.add(new Vent(layer, c(0,20)   ,  20.0 , 0));
+		list.add(new Vent(layer, c(0,-20)  ,  30.0 , 0));
+		list.add(new Vent(layer, c(20,0)   ,  40.0 , 0));
+		list.add(new Vent(layer, c(20,20)  ,  50.0 , 0));
+		list.add(new Vent(layer, c(20,-20) ,  60.0 , 0));
+		list.add(new Vent(layer, c(40,0)   ,  70.0 , 0));
+		list.add(new Vent(layer, c(40,20)  ,  80.0 , 0));
+		list.add(new Vent(layer, c(40,-20) ,  90.0 , 0));
+		list.add(new Vent(layer, c(0,40)    ,  100.0 , 0));
+		list.add(new Vent(layer, c(0,-40)   ,  110.0 , 0));
+		list.add(new Vent(layer, c(20,40)  ,  120.0 , 0));
+		list.add(new Vent(layer, c(20,-40)   ,  130.0 , 0));
+		list.add(new Vent(layer, c(40,40)  ,  140.0 , 0));
+		list.add(new Vent(layer, c(40,-40)  ,  150.0 , 0));
+	*/
+		
+		return list;
+	}
+    private static Coordinate c(double lat, double lon) {
+        return new Coordinate(lat, lon);
+    }
+	
+}
