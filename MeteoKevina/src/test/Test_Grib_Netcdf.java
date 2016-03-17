6aacde3b972d1e5e194dc66fa5f3558f7a5e4f27
Matthieu Quéré <matthieu.quere29@gmail.com>
2016-03-17 11:37:44 +0100

    Ajout test pour fichier grib et netcdf

---
+++ MeteoKevina/src/test/Test_Grib_Netcdf.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_Grib_Netcdf {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
